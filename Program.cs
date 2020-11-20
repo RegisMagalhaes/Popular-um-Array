a5ccc1fa8833c82921f39ab43822818c4e7fa6f2
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-20 12:10:25 -0300

    Este programa pergunta ao usuario o tamanho da lista e permite cadastrar itens para esta lista

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AlunosExercicioArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos alunos tem na sala?");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            string[] nomeAlunos = new string[tamanho];
+
+            for (var i = 0; i < nomeAlunos.Length; i++)
+            {
+                Console.Write("Digite um nome: ");
+                nomeAlunos[i] = Console.ReadLine();
+            }    
+            Console.WriteLine("A sala tem os seguintes alunos:");
+            for (var i = 0; i < nomeAlunos.Length; i++)
+            {
+                Console.WriteLine(nomeAlunos[i]);
+            }
+        }
+    }
+}
